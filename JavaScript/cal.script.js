225464a77dc1a938db7d4a684a4c67ca7400b218
zainabzia22 <zzia9851@gmail.com>
2026-07-17 22:04:17 -0700

    First commit

---
+++ JavaScript/cal.script.js
@@ -0,0 +1,23 @@
+let num1 = Number(prompt("Enter first number:"));
+let operator = prompt("Enter operator (+, -, *, /):");
+let num2 = Number(prompt("Enter second number:"));
+
+let result;
+
+if (operator === "+") {
+    result = num1 + num2;
+}
+else if (operator === "-") {
+    result = num1 - num2;
+}
+else if (operator === "*") {
+    result = num1 * num2;
+}
+else if (operator === "/") {
+    result = num1 / num2;
+}
+else {
+    result = "Invalid operator";
+}
+
+console.log("Result:", result);
