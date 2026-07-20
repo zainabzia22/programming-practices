a6b23ded0ec34d66b318553619461ae4c22465c7
zainabzia22 <zzia9851@gmail.com>
2026-07-19 22:33:31 -0700

    Add Student Result System and Shopping Cart programs

---
+++ JavaScript/student-result-system.js
@@ -0,0 +1,47 @@
+const students = [
+  { name: "Ali", marks: 85 },
+  { name: "Ahmed", marks: 42 },
+  { name: "Sara", marks: 76 },
+  { name: "Ayesha", marks: 91 },
+  { name: "Bilal", marks: 58 }
+];
+// Display all students
+console.log("Student List:");
+students.forEach(student => {
+  console.log(`${student.name}: ${student.marks}`);
+});
+// Passed Students
+const passedStudents = students.filter(student => student.marks >= 50);
+
+console.log("\nPassed Students:");
+passedStudents.forEach(student => {
+  console.log(student.name);
+});
+// Grades
+const grades = students.map(student => {
+  let grade;
+
+  if (student.marks >= 80) {
+    grade = "A";
+  } else if (student.marks >= 70) {
+    grade = "B";
+  } else if (student.marks >= 60) {
+    grade = "C";
+  } else if (student.marks >= 50) {
+    grade = "D";
+  } else {
+    grade = "Fail";
+  }
+   return {
+    ...student,
+    grade
+  };
+});
+console.log("\nStudent Grades:");
+console.log(grades);
+
+// Average Marks
+const totalMarks = students.reduce((total, student) => total + student.marks, 0);
+const average = totalMarks / students.length;
+
+console.log("\nAverage Marks:", average);
